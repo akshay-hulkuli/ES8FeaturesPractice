2f1ea292536d2bd1033ce30c3a8e43cfb1cda2cf
Akshaya HP <akshayhulkuli@gmail.com>
2021-12-06 11:55:55 +0530

    [Akshay]ADDED: code to demonstrate string padding

---
+++ string_padding.js
@@ -0,0 +1,21 @@
+let product_cost = '244'.padStart(6,0)
+console.log(product_cost)
+console.log(product_cost.length)
+
+
+let str = 'world'.padStart(15,'hello')
+console.log(str)
+console.log(str.length)
+
+
+
+let product_cost1 = '244'.padEnd(6,0)
+console.log(product_cost1)
+console.log(product_cost1.length)
+
+
+let str1 = 'world'.padEnd(15,'hello')
+console.log(str1)
+console.log(str1.length)
+
+
